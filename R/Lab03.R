beb3e5a1e77c2bcb0fc071854263ba3e48300fe9
José Jaime Méndez Flores <you@example.com>
2019-09-20 23:34:53 +0200

    Changes to wikigraph documentation!

--- R/Lab03.R
+++ R/Lab03.R
@@ -73,3 +73,16 @@ dijkstra <- function(graph, init_node){
   }
   return (dist)
 }
+
+#' Distances w between 6 nodes v.
+#'
+#' A dataset containing the pathway showed on top of the wikipedia page from Dijkstra's algorithm
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{v1}{point 1}
+#'   \item{v2}{point 2}
+#'   \item{w}{weight or distance from point 1 to point 2}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra's algorithm}
+"wiki_graph"
