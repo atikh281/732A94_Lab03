e852815460edcf979d8a35d71c131310f8080b05
José Jaime Méndez Flores <you@example.com>
2019-09-20 19:55:43 +0200

    Added Dijkstra algorithm and documentation

--- R/Lab03.R
+++ R/Lab03.R
@@ -33,5 +33,43 @@ euclidean <- function (x, y){
   return(gcd)
   }
 
+#'@title Implement the dijkstra algorithm
+#'
+#'@param graph, A data frame with three variables, v1, v2 and w that contains the edges of the graph (fromv1 to v2) with the weight of the edge (w)
+#'@param init_node, A number.
+#'@description The function returns the shortest path to every other node in \code{graph} from \code{init_node}.
+#'
+#'@references More information of dijkstra algorithm \href{https://en.wikipedia.org/wiki/Dijkstra%27s}{here}
+#'@export
+
 #1.1.2 dijkstra
+
+dijkstra <- function(graph, init_node){
+  Q <- c()
+  dist <- c()
+  prev <- c()
+  
+  for (i in 1:length(unique(graph[[1]]))){
+    dist[i] <- Inf
+    prev[i] <- NA
+    Q[i] <- unique(graph[[1]])[i]
+  }
+  
+  dist[init_node] <- 0
+  
+  #Init node closest neighbor
+  while (length(Q) > 1){
+    u <- Q[which.min(dist[Q])]
+    Q <- Q[Q!=u]
+    for(i in 1:sum(graph[graph$v1 == u,]$v2 %in% Q)){
+      alt <- dist[u] + graph[graph$v1 == u,][graph[graph$v1 == u,]$v2 %in% Q,]$w[i]
+      if (alt < dist[graph[graph$v1 == u,][graph[graph$v1 == u,]$v2 %in% Q,]$v2[i]]){
+        dist[graph[graph$v1 == u,][graph[graph$v1 == u,]$v2 %in% Q,]$v2[i]] <- alt
+        prev[graph[graph$v1 == u,][graph[graph$v1 == u,]$v2 %in% Q,]$v2[i]] <- u
+      }
+    }
+  }
+  
+  return (dist, prev)
+}
 
